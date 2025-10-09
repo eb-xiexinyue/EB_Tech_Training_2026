ff987ed8217014f339b39d81b2a719e551a9b1ad
卲 永陽 <ei-sho@dts.co.jp>
2025-10-09 17:38:04 +0900

    test commit and push

--- src/co/jp/chapter01/JavaHomework01.java
+++ src/co/jp/chapter01/JavaHomework01.java
@@ -3,8 +3,7 @@ package co.jp.chapter01;
 public class JavaHomework01 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+		System.out.println("test commit or push");
 	}
 
 }
