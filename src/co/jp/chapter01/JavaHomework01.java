e0a2b73044ef3ab8a3ef52eaf9ecf74695545ac1
chaoweiwng <skykeir@gmail.com>
2025-10-16 23:24:04 +0900

    Homework1

--- src/co/jp/chapter01/JavaHomework01.java
+++ src/co/jp/chapter01/JavaHomework01.java
@@ -3,8 +3,25 @@ package co.jp.chapter01;
 public class JavaHomework01 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+		System.out.println("=== 宿題（1回目）===");
+		System.out.println("【質問1】");
+		System.out.println("n=0");
+		System.out.println();
+		
+		System.out.println("【質問2】");
+		System.out.println("もしa>10がtrueであれば、値11を、"
+				+ "逆にfalseの場合、値０をaに代入する。");
+		System.out.println();
+		
+		System.out.println("【質問3】");
+		System.out.println("==");
+		System.out.println();
+		
+		System.out.println("【質問4】");
+		System.out.println("「true」と「false」");
+		System.out.println();
+		
+		System.out.println("===以上、解答です===");
 	}
 
 }
