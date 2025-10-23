78a85eaf208abbcf3e1228794e65a42ee992aebd
lsy0417 <lsy0417@192.168.0.24>
2025-10-23 16:04:16 +0900

    homework01

--- src/co/jp/chapter01/JavaChapter01.java
+++ src/co/jp/chapter01/JavaChapter01.java
@@ -4,7 +4,12 @@ public class JavaChapter01 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
+		int x = 10; int y = 20;
+		x = y;
+		y = 30;
+		System.out.println(x);
+		System.out.println(y);
+		
 	}
 
 }
--- src/co/jp/chapter01/JavaHomework01.java
+++ src/co/jp/chapter01/JavaHomework01.java
@@ -4,7 +4,28 @@ public class JavaHomework01 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
+   
+		//質問1：以下変数nの値は？　
+        int i = 10;
+        int n = i%5;
+        System.out.println("nの値は"+n);
+        
+		//質問２︓ 右の式を解釈してください: a = a > 10 ? 11 : 0; 
+        // ①　a > 10 (条件がtrue)　の場合、a　＝　11；
+        int a = 15;
+        a = a >10 ? 11 : 0;
+        System.out.println("aの値は"+a);
+        
+        // ②　a > 10 (条件がfalse)　の場合、a　＝　0；
+        a = 5;
+        a = a > 10 ? 11 : 0;
+        System.out.println("aの値は"+a);
+        
+		//質問３︓変数値を⽐較する演算⼦は = or == ? 
+        //答え：＝＝
+        
+		//質問４︓ブリアン（ boolean ）変数に設定可能の値は「?」「?」。
+        //答え　：　「true」　と　「false」です。
 	}
 
 }
