cb3ba450417e3daa6307b5bece4d14c9ec2ea6e4
魏 立博 < @Wribo>
2025-11-13 02:36:00 +0900

    homework06

--- src/co/jp/chapter06/JavaChapter06.java
+++ src/co/jp/chapter06/JavaChapter06.java
@@ -1,10 +1,13 @@
 package co.jp.chapter06;
 
 public class JavaChapter06 {
+    private static void sayHello(String y) {
+        y = "Hello word";
+    }
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
-	}
-
+    public static void main(String[] args) {
+        String x = null;
+        sayHello(x);
+        System.out.println(x);
+    }
 }
